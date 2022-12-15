179ea1d92dfce3fd8b4e3a3fddf1bb4b01e7652f
Miguel Fernandez-Cortizas <miferco97@gmail.com>
2022-12-16 00:45:17 +0100

    day 13 part 1 completed

--- day13_distress_signal/find_ordered_packages.py
+++ day13_distress_signal/find_ordered_packages.py
@@ -1,48 +1,88 @@
+from json import loads
 import os
 import sys
-from typing import List, Tuple
+from typing import List, Tuple, Union
+
+
+def find_pair(string: str, index: int = 0) -> Tuple:
+    line = string[index:]
+    depth = 0
+    for i, char in enumerate(line):
+        if char == '[':
+            depth += 1
+        elif char == ']':
+            depth -= 1
+            if depth == 0:
+                return (index, i+index)
+    return (-1, -1)
 
 
 def parse_list(line: str) -> List:
-    line = line.strip()
-    # line_content = []
-    print('line',line)
+    line = line.strip()[1:-1]
     index = 0
-    oppening=[]
+    oppening = []
     brackets_list = []
 
+    digits = []
     while index < len(line):
         char = line[index]
         if char == '[':
-            oppening.append(index)
-            print('line',line , 'index',index)
-        elif char == ']':
-            begin_index = oppening.pop()
-            # sub_list =  line[begin_index:index+1]
-            if len(oppening) != 0:
-                sub_list =  parse_list(line[begin_index:index+1])
-                index = line.find(']',index) 
-                print('sub_list' , sub_list, ' :len ', len(sub_list))
-                brackets_list.append(sub_list)
+            begin, end = find_pair(line, index)
+            sub_str = line[begin:end+1]
+            sub_list = parse_list(sub_str)
+            index = end
+            brackets_list.append(sub_list)
         else:
-            if char not in [',', '[', ']'] and len(oppening) == 1:
-                brackets_list.append(char)
+            if char not in [',', '[', ']']:
+                digits.append(char)
+            elif char in [',', ']']:
+                if len(digits):
+                    number = int(''.join(digits))
+                    brackets_list.append(number)
+                    digits.clear()
         index += 1
-
+    if digits:
+        number = int(''.join(digits))
+        brackets_list.append(number)
     return brackets_list
 
 
 def parse_packages(filename: str) -> List:
     packages = []
     with open(filename, 'r') as f:
-        lines = f.readlines()
-        # parsed =parse_list(lines[3])
-        parsed = parse_list(lines[22])
-        print('parsed',parsed)
-        # for line in
-        #     pass
-
+        package = []
+        for line in f.readlines():
+            if not line.strip():
+                packages.append(tuple(package.copy()))
+                package.clear()
+            else:
+                package.append(parse_list(line))
+        packages.append(tuple(package.copy()))
     return packages
+
+
+def is_ordered(lhs, rhs) -> Union[bool, None]:
+    # print(lhs, rhs)
+    if isinstance(lhs, int) and isinstance(rhs, int):
+        if lhs == rhs:
+            # print('equal')
+            return None
+        return True if lhs < rhs else False
+    elif isinstance(lhs, List) and isinstance(rhs, List):
+        ordered = True
+        for i in range(min(len(lhs), len(rhs))):
+            value = is_ordered(lhs[i], rhs[i])
+            # print(value)
+            if value is not None:
+                return value
+        if len(rhs) == len(lhs):
+            return None
+        return True if len(lhs) < len(rhs) else False
+    elif isinstance(lhs, int) and isinstance(rhs, List):
+        return is_ordered([lhs], rhs)
+    elif isinstance(lhs, List) and isinstance(rhs, int):
+        return is_ordered(lhs, [rhs])
+    return False
 
 
 if __name__ == "__main__":
@@ -55,3 +95,13 @@ if __name__ == "__main__":
         sys.exit(1)
 
     packages = parse_packages(input_file)
+
+    packages = parse_packages(input_file)
+    right_order_list = []
+
+    for i, (lhs, rhs) in enumerate(packages, start=1):
+        value = is_ordered(lhs, rhs)
+        if value:
+            right_order_list.append(i)
+    print('The correct indices were:', right_order_list)
+    print('Their sum is:', sum(right_order_list))

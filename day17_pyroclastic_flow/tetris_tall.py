b0d35bf2497d4fe55def71510e55fc6ed5e2a283
Miguel <miferco97@gmail.com>
2022-12-22 20:48:52 +0100

    day 17 part1  completed

--- day17_pyroclastic_flow/tetris_tall.py
+++ day17_pyroclastic_flow/tetris_tall.py
@@ -2,7 +2,7 @@ import os
 import sys
 from itertools import cycle
 from typing import List, Tuple, Union, Any, Dict
-
+from tqdm import tqdm
 
 class Rock:
     def __init__(self, sprite: List, initial_position: Tuple) -> None:
@@ -13,10 +13,18 @@ class Rock:
     @property
     def left_limit(self):
         return self.position[0]
+    
+    @property
+    def height(self):
+        return len(self.sprite)
+
+    @property
+    def length(self):
+        return len(self.sprite[0])
 
     @property
     def right_limit(self):
-        return self.position[0] + len(self.sprite[0])
+        return self.position[0] + len(self.sprite[0])-1
 
     @property
     def top_limit(self):
@@ -26,64 +34,94 @@ class Rock:
     def bottom_limit(self):
         return self.position[1]
 
-    def move(self, direction: str):
-        pass
+    def move(self, direction:Tuple[int,int], rocks_lists):
+        new_x = self.position[0] + direction[0]
+        if new_x < 0 or new_x+self.length > 7:
+            return False
+        new_y = self.position[1] + direction[1]
+        if new_y < 0:
+            return False
+        if len(rocks_lists) > 100:
+            for rock in rocks_lists[-99:-1]:
+                if rock.check_collision(Rock(self.sprite,(new_x,new_y))):
+                    # print(f'returning False rock, collision_pixel = {(new_x,new_y)}')n
+                    return False
+        else:
+            for rock in rocks_lists[:-1]:
+               if rock.check_collision(Rock(self.sprite,(new_x,new_y))):
+                    # print(f'returning False rock, collision_pixel = {(new_x,new_y)}')n
+                    return False
+        self.position = (self.position[0] + direction[0], self.position[1] + direction[1])
+        return True
 
+    def check_position(self, position):
+        if position[1] > self.top_limit+1:
+            return False
+        # print(self.sprite)
+        for px,x in enumerate(range(self.left_limit,self.right_limit+1)):
+            for py,y in enumerate(range(self.bottom_limit,self.top_limit+1)):
+                if self.sprite[py][px] != '.' and (x == position[0] and y == position[1]):
+                    # print(f'{px=},{py=} = {self.sprite[py][px]} collide with {position}')
+                    return True
+        return False
+
+    def check_collision(self, other):
+        if other.top_limit +1 < self.bottom_limit:
+            return False
+        for px,x in enumerate(range(self.left_limit,self.right_limit+1)):
+            for py,y in enumerate(range(self.bottom_limit,self.top_limit+1)):
+                if self.sprite[py][px] != '.' and other.check_position((x,y)):
+                    return True
+        return False
 
 class Chamber:
     def __init__(self) -> None:
         self.rocks: List[Rock] = []
-        self.chamber: List = ['-------']
-        self.top_line = [0]*7
-        print(self.top_line)
+        self.chamber: List = []
+        self.down=False
+
+    @property
+    def higher_y(self):
+        return max([rock.top_limit for rock in self.rocks])+1 if self.rocks else 0
+
 
     def add_rock(self, sprite: List):
-        print(f'adding_rock: {sprite}')
-        initial_y = len(self.chamber)+3
-        self.rocks.append(Rock(sprite, (2, initial_y)))
-        self.chamber.extend(['.......']*(len(sprite)+3))
-
-    def check_end(self, rock: Rock):
-        end = False
-        for index, values in enumerate(self.top_line):
-            if index in range(rock.left_limit, rock.right_limit):
-                if rock.bottom_limit <= values+1:
-                    rock.has_ended = True
-                    end = True
-                    self.top_line[index] = rock.top_limit
-        return end
+        self.rocks.append(Rock(sprite, (2, self.higher_y+3)))
+        if len(self.chamber) < self.higher_y:
+            self.chamber.extend(['.......']*(self.higher_y - len(sprite)+2))
+        self.down =False
+        # print('new rock appeared:')
+        # print(self)
 
     def step(self, movement_cycle) -> bool:
         rock = self.rocks[-1]
         if rock.has_ended:
             return True
-        movement = movement_cycle.__next__()
-        if movement == '>' and rock.right_limit != len(self.chamber[0]):
-            rock.position = (rock.position[0]+1, rock.position[1])
-        elif movement == '<' and rock.left_limit != 0:
-            rock.position = (rock.position[0]-1, rock.position[1])
-        rock.position = (rock.position[0], rock.position[1]-1)
-        if (self.check_end(rock)):
-            movement = movement_cycle.__next__()
-            if movement == '>' and rock.right_limit != len(self.chamber[0]):
-                rock.position = (rock.position[0]+1, rock.position[1])
-            elif movement == '<' and rock.left_limit != 0:
-                rock.position = (rock.position[0]-1, rock.position[1])
-            return True
+        if self.down:
+            self.down=False
+            if rock.move((0,-1),self.rocks):
+                # print('falling')
+                return False
+            else:
+                rock.has_ended=True
+                return True
+        self.down=True
+        movement = next(movement_cycle)
+        movement_dict={'>':(1,0),'<':(-1,0)}
+        rock.move(movement_dict[movement],self.rocks)
         return False
 
     def __repr__(self) -> str:
         draw = ''
         basic_line = '.......'
         chamber: List = self.chamber.copy()
-        # if len(self.rocks):
-        #     chamber = [basic_line for x in range(self.rocks[-1].top_limit)]
         for rock in self.rocks:
             for i in range(rock.bottom_limit, rock.top_limit+1):
                 line = chamber[i]
                 new_line = ''
                 for x, char in enumerate(line):
-                    if x in range(rock.left_limit, rock.right_limit):
+                    if x in range(rock.left_limit, rock.right_limit+1) and \
+                            rock.sprite[i-rock.bottom_limit][x-rock.left_limit] != '.' :
                         if rock.has_ended:
                             new_line = new_line+'#'
                         else:
@@ -95,7 +133,7 @@ class Chamber:
         chamber.reverse()
         for line in chamber:
             draw = draw + '|' + ' '.join(line) + '|' + '\n'
-        # draw = draw + ' '.join('+-------+')
+        draw = draw + ' '.join('+-------+')
         return draw
 
 
@@ -117,19 +155,22 @@ if __name__ == "__main__":
     with open('./rock_types', 'r') as f:
         rock_types_list = [y.split('\n')
                            for y in [x for x in f.read().split('\n\n')]]
+        for _rock in rock_types_list:
+            _rock.reverse()
     rock_types = cycle(rock_types_list)
-
     chamber = Chamber()
 
-    for n_rock in range(6):
-        sprite = rock_types.__next__()
+    for n_rock in tqdm(range(2022)):
+        sprite = next(rock_types)
+        # print(sprite)
         chamber.add_rock(sprite.copy())
         while(not chamber.step(commands)):
-            print(chamber)
-            # pass
-        print(chamber)
+            # print(chamber)
+            pass
+        # print(chamber)
+    print(chamber.higher_y)
     # chamber.add_rock(['@@@@'])
-    print(chamber)
+    # print(chamber)
     # chamber.step('<')
     # print(chamber)
     # chamber.step('<')
